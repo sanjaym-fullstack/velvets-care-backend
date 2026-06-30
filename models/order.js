16a66c52a127bdf02c0a481d937e375075f1b0f8
Debanjan Dasgupta <debanjandasgupta007@gmail.com>
2026-06-30 22:47:13 +0530

    Order address added successfullt

--- models/order.js
+++ models/order.js
@@ -4,28 +4,35 @@ const {
   DataTypes
 } = require('sequelize');
 const {
- tables: {
+  tables: {
     Orders
   },
   sequelize
 } = require('../config');
-  
 
-  class Order extends Model {
-  }
-  Order.init({
-    user_id: DataTypes.INTEGER,
-    total_amount: DataTypes.DOUBLE,
-    status: DataTypes.STRING,
-    payment_status: DataTypes.STRING,
-    payment_method: DataTypes.STRING,
-    discount_code: DataTypes.STRING,
-    notes: DataTypes.JSON
-  }, {
-    sequelize,
-    paranoid: true,
-    modelName: Orders,
-    tableName: Orders,
-  });
+const addressModel = require('./address');
+
+
+class Order extends Model {
+}
+Order.init({
+  user_id: DataTypes.INTEGER,
+  address_id: DataTypes.INTEGER,
+  total_amount: DataTypes.DOUBLE,
+  status: DataTypes.STRING,
+  payment_status: DataTypes.STRING,
+  payment_method: DataTypes.STRING,
+  discount_code: DataTypes.STRING,
+  notes: DataTypes.JSON
+}, {
+  sequelize,
+  paranoid: true,
+  modelName: Orders,
+  tableName: Orders,
+});
+
+addressModel.hasMany(Order, { foreignKey: 'address_id' });
+Order.belongsTo(addressModel, { foreignKey: 'address_id' });
+
 
 module.exports = Order;
