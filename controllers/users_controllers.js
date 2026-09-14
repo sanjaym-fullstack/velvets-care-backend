7fd5d45d0f703ec76377909d2b9d05855cd859d4
sanjugowdam <sanjaym3236@gmail.com>
2026-09-14 11:13:20 +0530

    Fix profile image not passing in getusers and update_user - use user.Files instead of user.profile_image

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -381,8 +381,8 @@ const update_user = async (req, res) => {
         // Map S3 URL
         const user_data = {
             ...updatedUser,
-            profile_image: updatedUser.profile_image?.files_url
-                ? await FileFunctions.getFromS3(updatedUser.profile_image.files_url)
+            profile_image: updatedUser.Files?.files_url
+                ? await FileFunctions.getFromS3(updatedUser.Files.files_url)
                 : null
         };
 
@@ -493,8 +493,8 @@ const getusers = async (req, res) => {
         // Map S3 URLs for profile images
         const users_mapped = await Promise.all(users.map(async (user) => ({
             ...user,
-            profile_image: user.profile_image?.files_url
-                ? await FileFunctions.getFromS3(user.profile_image.files_url)
+            profile_image: user.Files?.files_url
+                ? await FileFunctions.getFromS3(user.Files.files_url)
                 : null
         })));
 
